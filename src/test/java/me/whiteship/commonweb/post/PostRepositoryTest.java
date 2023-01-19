0a00381d00679234640e121aea99f7162f0c35c5
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-19 21:02:20 +0900

    스프링 데이터 JPA 2. JpaRepository.save() 메소드

--- src/test/java/me/whiteship/commonweb/post/PostRepositoryTest.java
+++ src/test/java/me/whiteship/commonweb/post/PostRepositoryTest.java
@@ -25,14 +25,20 @@ public class PostRepositoryTest {
     public void crud() {
         Post post = new Post();
         post.setTitle("jpa");
-        postRepository.save(post); //insert
+        Post savedPost = postRepository.save(post);//persist
 
-        assertThat(entityManager.contains(post));
+        assertThat(entityManager.contains(post)).isTrue();
+        assertThat(entityManager.contains(savedPost)).isTrue();
+        assertThat(savedPost).isSameAs(post);
 
         Post postUpdate = new Post();
         postUpdate.setId(post.getId());
         postUpdate.setTitle("jpa");
-        postRepository.save(postUpdate); //update
+        Post updatedPost = postRepository.save(postUpdate);//merge
+
+        assertThat(entityManager.contains(postUpdate)).isFalse();
+        assertThat(entityManager.contains(updatedPost)).isTrue();
+        assertThat(updatedPost).isNotSameAs(postUpdate);
 
         List<Post> all = postRepository.findAll();
         assertThat(all.size()).isEqualTo(1);
