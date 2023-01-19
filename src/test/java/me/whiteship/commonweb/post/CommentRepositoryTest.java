68ff6bed09dcca1dbb8aabefc3b7c1fca9fc3b1e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-19 22:49:54 +0900

    스프링 데이터 JPA 10. Query by Example

--- src/test/java/me/whiteship/commonweb/post/CommentRepositoryTest.java
+++ src/test/java/me/whiteship/commonweb/post/CommentRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -40,6 +42,19 @@ public class CommentRepositoryTest {
     @Test
     public void specs() {
         comments.findAll(isBest().or(isGood()), PageRequest.of(0, 10));
+    }
+
+    @Test
+    public void qbe() {
+        Comment prove = new Comment();
+        prove.setBest(true);
+
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withIncludeNullValues();
+
+        Example<Comment> example = Example.of(prove, exampleMatcher);
+
+        comments.findAll(example);
     }
 
 }
