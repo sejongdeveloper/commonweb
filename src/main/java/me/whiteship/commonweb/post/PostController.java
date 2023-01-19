68d7c7f882be2648300ac3ec180c030508eb3ece
sejongdeveloper <sejongdeveloper@gmail.com>
2023-01-19 09:06:29 +0900

    스프링 데이터 Common12. 웹 기능 2부 DomainClassConverter

--- src/main/java/me/whiteship/commonweb/post/PostController.java
+++ src/main/java/me/whiteship/commonweb/post/PostController.java
@@ -5,17 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 public class PostController {
     @Autowired
     private PostRepository postRepository;
 
     @GetMapping("/posts/{id}")
-    public String getPost(@PathVariable Long id) {
-        Optional<Post> byId = postRepository.findById(id);
-        Post post = byId.get();
+    public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
     }
 }
