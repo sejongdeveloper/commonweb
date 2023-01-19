fe789ebba13c74fc0d5b601bf5211d468bd8c6af
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-19 20:57:23 +0900

    스프링 데이터 JPA 2. JpaRepository.save() 메소드

--- src/test/java/me/whiteship/commonweb/post/PostRepositoryTest.java
+++ src/test/java/me/whiteship/commonweb/post/PostRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,11 +18,21 @@ public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Test
     public void crud() {
         Post post = new Post();
         post.setTitle("jpa");
-        postRepository.save(post);
+        postRepository.save(post); //insert
+
+        assertThat(entityManager.contains(post));
+
+        Post postUpdate = new Post();
+        postUpdate.setId(post.getId());
+        postUpdate.setTitle("jpa");
+        postRepository.save(postUpdate); //update
 
         List<Post> all = postRepository.findAll();
         assertThat(all.size()).isEqualTo(1);
