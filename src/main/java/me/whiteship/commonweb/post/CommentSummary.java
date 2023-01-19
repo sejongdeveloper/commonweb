9af286e064d2305ad0597a75d7f94e8c484c5ded
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-19 22:23:13 +0900

    스프링 데이터 JPA 8. Projection

--- src/main/java/me/whiteship/commonweb/post/CommentSummary.java
+++ src/main/java/me/whiteship/commonweb/post/CommentSummary.java
@@ -2,15 +2,44 @@ package me.whiteship.commonweb.post;
 
 import org.springframework.beans.factory.annotation.Value;
 
-public interface CommentSummary {
-    String getComment();
+public class CommentSummary {
+    private String comment;
 
-    int getUp();
+    private int up;
 
-    int getDown();
+    private int down;
 
-    default String getVotes() {
+    public CommentSummary(String comment, int up, int down) {
+        this.comment = comment;
+        this.up = up;
+        this.down = down;
+    }
+
+    public String getVotes() {
         return getUp() + " " + getDown();
     }
 
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getUp() {
+        return up;
+    }
+
+    public void setUp(int up) {
+        this.up = up;
+    }
+
+    public int getDown() {
+        return down;
+    }
+
+    public void setDown(int down) {
+        this.down = down;
+    }
 }
