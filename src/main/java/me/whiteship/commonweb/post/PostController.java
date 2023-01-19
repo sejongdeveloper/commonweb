19c2a8c9d8ddbbaf933839bacbbfc0922b0b2947
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-19 20:20:17 +0900

    스프링 데이터 Common 13.웹 기능 3부 Pageable과 Sort

--- src/main/java/me/whiteship/commonweb/post/PostController.java
+++ src/main/java/me/whiteship/commonweb/post/PostController.java
@@ -1,6 +1,8 @@
 package me.whiteship.commonweb.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostController {
     @Autowired
-    private PostRepository postRepository;
+    private PostRepository posts;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+        return posts.findAll(pageable);
     }
 }
