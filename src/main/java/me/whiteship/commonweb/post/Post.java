455978abffa64fb03d98e861eb520273c130894e
sejongdeveloper <sejongdeveloper@gmail.com>
2023-01-19 09:04:41 +0900

    스프링 데이터 Common12. 웹 기능 2부 DomainClassConverter

---
+++ src/main/java/me/whiteship/commonweb/post/Post.java
@@ -0,0 +1,43 @@
+package me.whiteship.commonweb.post;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+public class Post {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
---
+++ src/main/java/me/whiteship/commonweb/post/PostController.java
@@ -0,0 +1,21 @@
+package me.whiteship.commonweb.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class PostController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id) {
+        Optional<Post> byId = postRepository.findById(id);
+        Post post = byId.get();
+        return post.getTitle();
+    }
+}
