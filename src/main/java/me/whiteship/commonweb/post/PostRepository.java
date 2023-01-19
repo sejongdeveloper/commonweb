6afd8c16e1a320dc67c127d5a9fb1a8711c5eccc
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-19 21:22:02 +0900

    스프링 데이터 JPA 5. Named Parameter와 SpEL

--- src/main/java/me/whiteship/commonweb/post/PostRepository.java
+++ src/main/java/me/whiteship/commonweb/post/PostRepository.java
@@ -3,12 +3,13 @@ package me.whiteship.commonweb.post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleStartingWith(String title);
 
-    @Query("select p from Post as p where p.title = ?1")
-    List<Post> findByTitle(String title, Sort sort);
+    @Query("select p from Post as p where p.title = :title")
+    List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 }
